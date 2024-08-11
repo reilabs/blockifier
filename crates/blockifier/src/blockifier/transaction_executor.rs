a8a651abc1bb611f86175e0db5a2082329de161d
Eagle941 <8973725+Eagle941@users.noreply.github.com>
2024-08-12 00:48:24 +0100

    Added reference to `VisitedPcs` in `blockifier/transaction_executor.rs`.

--- crates/blockifier/src/blockifier/transaction_executor.rs
+++ crates/blockifier/src/blockifier/transaction_executor.rs
@@ -13,7 +13,9 @@ use crate::bouncer::{Bouncer, BouncerWeights};
 #[cfg(feature = "concurrency")]
 use crate::concurrency::worker_logic::WorkerExecutor;
 use crate::context::BlockContext;
-use crate::state::cached_state::{CachedState, CommitmentStateDiff, TransactionalState};
+use crate::state::cached_state::{
+    CachedState, CommitmentStateDiff, TransactionalState, VisitedPcs,
+};
 use crate::state::errors::StateError;
 use crate::state::state_api::StateReader;
 use crate::transaction::errors::TransactionExecutionError;
